f2e94e92589f344f712b5918376a37b1d29cd784
rocinate <rocinate1999@gmail.com>
2023-03-30 22:48:04 +0800

    cpp不断完善中
    Co-authored-by: Stepend <Stepend@users.noreply.github.com>
    Co-authored-by: chengyaozong <ankh1999@users.noreply.github.com>

--- include/robot.h
+++ include/robot.h
@@ -1,8 +1,10 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 
+#include <string>
+#include <vector>
 #include "vec.h"
-
+#include "actionModel.h"
 /*
  * @brief 机器人代理类
  *
@@ -19,17 +21,25 @@ private:
     int lastProductType;          // 上一帧携带的品
     double timeCoefficients;      // 时间价值系数 [0.8, 1]
     double collisionCoefficients; // 碰撞价值系数 [0.8, 1]
-    double angularVelocity;       // 角速度 单位：弧度每秒， 正数表示顺时针， 负数表示逆时针
+    double w;                     // 角速度 单位：弧度每秒， 正数表示顺时针， 负数表示逆时针
     double heading;               // 朝向 [-pi, pi] 0 表示右方向, pi/2表示上方向
+    int leftFrame;                // 剩余帧数
     Vec pos;                      // 机器人坐标位置
     Vec velocity;                 // 线速度， 二维向量描述, m/s
+    ActionModel actionModel;      // 机器人动作模型
 
 public:
-    Robot(int id, Vec pos);
+    Robot(int id, double x, double y);
 
-    void updatePid();
-    void control();
+    int getId() const;
     double getPriority() const;
+    Vec getPos() const;
+    Vec getVelocity() const;
+
+    void checkDeal();
+    void update(int leftFrame);
+    void updatePid(int count);
+    void control();
 
     bool operator<(const Robot &o) const; // 重载为降序排列
 };
